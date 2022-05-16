e28f7eb0d1b817d6f1511e1fa9a56726eee2af53
Doyoon Lee <dynleedev@gmail.com>
2022-05-16 17:21:30 +0900

    Clean code

--- buddy-memory-allocator.py
+++ buddy-memory-allocator.py
@@ -1,9 +1,10 @@
-# Set total memory space
+# Set the total memory space
 memory = 64
 
-# Initialize memory, li_memory = [[starting index, size, used], ...]
+# Initialize the memory, li_memory = [[starting index, size of memory, flag for used or not], ...]
 li_memory = [[0, memory, False]]
 
+# Function to print the whole memory
 def print_blocks():
     print('|', end='')
     for i in li_memory:
@@ -28,29 +29,27 @@ while(1):
         elif (blocks > max([x[1] for x in li_memory if x[2] == False])):
             print("You can't allocate more than the biggest free blocks")
         else:
-            #  Find the minimum free space
-            min_memory_space = min([mmy[1] for mmy in li_memory if mmy[1]>=blocks and mmy[2] == False])
+            #  Find the minimum free space which is at least same or bigger than the number of blocks
+            min_memory_space = min([space[1] for space in li_memory if space[1]>=blocks and space[2] == False])
 
             # Get the index of the minimum memory space which is not allocated
-            idx = [(y[1],y[2]) for y in li_memory].index((min_memory_space,False))
+            idx = [(space[1],space[2]) for space in li_memory].index((min_memory_space,False))
             
             # Split the memory space
             while(min_memory_space > blocks):
-                # min(li_memory, key=itemgetter(1))[1] 이런거 사용해서 clean code
                 print(f'(splitting {li_memory[idx][0]}/{li_memory[idx][1]})')
                 
-                # Split the memory space by 2
+                # Split the memory space by 2 if the space is same or bigger than the number of blocks * 2
                 if (li_memory[idx][1]//2 >= blocks and li_memory[idx][1]%2 == 0):
                     li_memory[idx:idx+1] = [[li_memory[idx][0], li_memory[idx][1]//2, False],[li_memory[idx][0]+li_memory[idx][1]//2, li_memory[idx][1]//2, False]]
-                    # li_memory=li_memory[:idx]+[[li_memory[idx][0], li_memory[idx][1]//2, False],[li_memory[idx][0]+li_memory[idx][1]//2, li_memory[idx][1]//2, False]]+li_memory[idx+1:]
                 
-                # Split the memory space by the number of blocks
+                # Split the memory space by the number of blocks if the space is smaller than the number of blocks * 2
                 else:
                     li_memory[idx:idx+1] = [[li_memory[idx][0],blocks,False],[li_memory[idx][0]+blocks,li_memory[idx][1]-blocks,False]]
-                    # li_memory=li_memory[:idx]+[[li_memory[idx][0],blocks,False],[li_memory[idx][0]+blocks,li_memory[idx][1]-blocks,False]]+li_memory[idx+1:]
                 
                 # Update the minimum memory space
-                min_memory_space = min([mmy[1] for mmy in li_memory if mmy[1]>=blocks and mmy[2] == False])
+                min_memory_space = li_memory[idx][1]
+
             
             # Allocate the number of blocks to the memory space
             li_memory[idx][2] = True
@@ -81,19 +80,16 @@ while(1):
                 curr_idx_start = li_memory[curr_idx][0]
                 curr_idx_size = li_memory[curr_idx][1]
 
-                # If left and right neighbor are both empty
+                # If left and right neighbors are both empty
                 if ((curr_idx!=0 and curr_idx!=len(li_memory)-1) and li_memory[curr_idx-1][2]==False and li_memory[curr_idx+1][2]==False):
-                    # print("in both")
                     target = curr_idx-1 if li_memory[curr_idx-1][1]<=li_memory[curr_idx+1][1] else curr_idx+1
                 # If only left neighbor is empty
                 elif (curr_idx!=0 and li_memory[curr_idx-1][2]==False):
-                    # print("in L")
                     target = curr_idx-1
                 # If only right neighbor is empty
                 elif (curr_idx!=len(li_memory)-1 and li_memory[curr_idx+1][2]==False):
-                    # print("in R")
                     target = curr_idx+1
-                # Stop merging cuz no neighbor is empty
+                # Stop merging since no neighbor is empty
                 else:
                     break
                 
